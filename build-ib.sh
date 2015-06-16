4a6185cb92ba35bfea3202dff5541d5dc67ce24e
Dario Berzano <dario.berzano@cern.ch>
2015-06-16 16:08:43 +0200

    GAR builder: optional SVN recipe user/pass

--- build-ib.sh
+++ build-ib.sh
@@ -13,6 +13,8 @@ Usage:
   build-ib.sh.sh --software <software_name> \
                  --recipe-version <recipe_version> \
                  [--recipe-base-url <base_svn_url>] \
+                 [--recipe-svn-user <recipe_svn_user>] \
+                 [--recipe-svn-pass <recipe_svn_pass>] \
                  [--ncores <make_j_cores>]
 
 Example:
@@ -23,6 +25,8 @@ Alternatively it possible to specify parameters as envvars:
   --software        --> DEFAULT_RECIPE_SW
   --recipe-version  --> DEFAULT_RECIPE_VER
   --recipe-base-url --> DEFAULT_RECIPE_VER
+  --recipe-svn-user --> DEFAULT_RECIPE_USER
+  --recipe-svn-pass --> DEFAULT_RECIPE_PASS
   --ncores          --> DEFAULT_NCORES
 
 EoF
@@ -35,6 +39,8 @@ RecipeSw="${DEFAULT_RECIPE_SW:-}"
 RecipeVer="${DEFAULT_RECIPE_VER:-}"
 MakeCores=${DEFAULT_NCORES:-$( echo 'scale=2;a='`grep -c bogomips /proc/cpuinfo`'*1.05;scale=0;a/1' | bc )}
 RecipeBaseUrl=${DEFAULT_RECIPE_BASE_URL:-'http://svn.cern.ch/guest/aliroot-bits/branches'}
+RecipeSvnUser="${DEFAULT_RECIPE_USER:-}"
+RecipeSvnPass="${DEFAULT_RECIPE_PASS:-}"
 
 while [[ $# -gt 0 ]] ; do
   case "$1" in
@@ -42,6 +48,8 @@ while [[ $# -gt 0 ]] ; do
     --recipe-version) RecipeVer="$2" ; shift 2 ;;
     --recipe-base-url) RecipeBaseUrl="$2" ; shift 2 ;;
     --ncores) MakeCores="$2" ; shift 2 ;;
+    --recipe-svn-user) RecipeSvnUser="$2" ; shift 2 ;;
+    --recipe-svn-pass) RecipeSvnPass="$2" ; shift 2 ;;
     *) shift ;;
   esac
 done
@@ -71,7 +79,13 @@ mkdir -p "$RegisterTarballsDir" "$FinalTarballsDir"
 ln -nfs "$WwwDir" "$AltWwwDir"
 
 # Download recipe
-svn checkout "$RecipeUrl" "$RecipeDir" --non-interactive
+if [[ "$RecipeSvnUser" != '' && "$RecipeSvnPass" != '' ]] ; then
+  svn checkout "$RecipeUrl" "$RecipeDir" \
+    --non-interactive \
+    --username "$RecipeSvnUser" --password "$RecipeSvnPass"
+else
+  svn checkout "$RecipeUrl" "$RecipeDir" --non-interactive
+fi
 
 # Configure GAR, the build recipes handler
 cd "$RecipeDir"
