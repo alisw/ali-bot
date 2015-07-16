9e3071033be227e5c7ae8e704dca465d14a94168
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-07-16 14:01:56 +0200

    Simplify results

--- script/get_builds.sh
+++ script/get_builds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -X POST http://elasticsearch.marathon.mesos:9200/_search/ -d@es/build_results.json > $WORKSPACE/alisw.github.io/_data/build.json
-curl -X POST http://elasticsearch.marathon.mesos:9200/_search/ -d@es/build_summary.json > $WORKSPACE/alisw.github.io/_data/build_summary.json
+curl -X POST "http://elasticsearch.marathon.mesos:9200/_search/?pretty=true&filter_path=**.key" -d@es/build_summary.json > $WORKSPACE/alisw.github.io/_data/build_summary.json
