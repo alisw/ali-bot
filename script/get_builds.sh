f266404ce96119d300ea4c480369b61de17eaec9
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-07-16 12:25:55 +0200

    Add query to get minimal build summary.

--- script/get_builds.sh
+++ script/get_builds.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -X POST http://elasticsearch.marathon.mesos:9200/_search/ -d@es/build_results.json
+curl -X POST http://elasticsearch.marathon.mesos:9200/_search/ -d@es/build_results.json > $WORKSPACE/alisw.github.io/_data/build.json
+curl -X POST http://elasticsearch.marathon.mesos:9200/_search/ -d@es/build_summary.json > $WORKSPACE/alisw.github.io/_data/build_summary.json
