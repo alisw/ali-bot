cf345532c02d3d4c659023b6cf2fc7ac10216364
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-07-17 16:00:29 +0200

    Add query for warnings.

--- script/get_builds.sh
+++ script/get_builds.sh
@@ -5,3 +5,4 @@ TARGET=${TARGET-$WORKSPACE/alisw.github.io}
 curl -X POST "http://$ES_HOST/_search/?pretty=true&filter_path=**.key" -d@es/build_results.json > $TARGET/_data/build.json
 curl -X POST "http://$ES_HOST/_search/?pretty=true&filter_path=**.key" -d@es/build_summary.json > $TARGET/_data/build_summary.json
 curl -X POST "http://$ES_HOST/_search/?pretty=true&filter_path=**.key" -d@es/build_errors.json > $TARGET/_data/errors.json
+curl -X POST "http://$ES_HOST/_search/?pretty=true&filter_path=**.key" -d@es/build_warnings.json > $TARGET/_data/warnings.json
