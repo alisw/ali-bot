fa2303e05f366a184f63d52779f607d78175a2f6
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-09-24 19:06:36 +0200

    Move to use elasticsearch-client
    This frees up data nodes from responding to queries, hopefully improving
    the reliability of the system.

--- script/get_builds.sh
+++ script/get_builds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ES_HOST=elasticsearch.marathon.mesos:9200
+ES_HOST=elasticsearch-client.marathon.mesos:9200
 PROXY=${PROXY+-x socks5h://$PROXY}
 TARGET=${TARGET-$WORKSPACE/alisw.github.io}
 curl -X POST "http://$ES_HOST/_search/?pretty=true&filter_path=**.key" -d@es/build_results.json > $TARGET/_data/build.json
--- script/set_mappings.sh
+++ script/set_mappings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ES_HOST=elasticsearch.marathon.mesos:9200
+ES_HOST=elasticsearch-client.marathon.mesos:9200
 PROXY=${PROXY+--proxy socks5h://$PROXY}
 TARGET=${TARGET-$WORKSPACE/alisw.github.io}
 curl $PROXY -X PUT "http://$ES_HOST/_mapping/logs" -d@es-mappings/logs.json
