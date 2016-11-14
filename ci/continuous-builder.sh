dfe179048f4208e3d5ef4555ec8b1fe1c21ca788
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-11-14 11:30:15 +0100

    Improve support for PR testing
    - Can test PR and branches
    - Reports PR errors as comments to the PR
    - Reports errors in branches as issues and closes them
      when the commit is outdated
    - Highly reduce the amount of API calls required

--- ci/continuous-builder.sh
+++ ci/continuous-builder.sh
@@ -13,48 +13,79 @@ popd
 ali-bot/set-github-status -c alisw/alidist@$ALIDIST_REF -s build/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/pending
 
 while true; do
-  for d in $(find . -maxdepth 2 -name .git -exec dirname {} \;); do
+  for d in $(find . -maxdepth 2 -name .git -exec dirname {} \; | grep -v ali-bot); do
     pushd $d
       git pull origin
     popd
   done
-  DOCTOR_ERROR=""
-  BUILD_ERROR=""
 
   if [[ "$PR_REPO" != "" ]]; then
-    HASHES=`ali-bot/list-branch-pr --trusted $TRUSTED_USERS $PR_REPO@$PR_BRANCH`
+    HASHES=`ali-bot/list-branch-pr --show-main-branch ${TRUSTED_USERS:+--trusted $TRUSTED_USERS} $PR_REPO@$PR_BRANCH`
   else
-    HASHES="0"
+    HASHES="0@0"
   fi
-  for pr_hash in $HASHES; do
+  for pr_id in $HASHES; do
+    DOCTOR_ERROR=""
+    BUILD_ERROR=""
+    pr_number=${pr_id%@*}
+    pr_hash=${pr_id#*@}
     if [[ "$PR_REPO" != "" ]]; then
       pushd `basename $PR_REPO`
-        git reset --hard $PR_BRANCH
+        CANNOT_MERGE=0
+        git reset --hard origin/$PR_BRANCH
         git config --add remote.origin.fetch "+refs/pull/*/head:refs/remotes/origin/pr/*"
         git fetch origin
         git clean -fxd
-        git merge $pr_hash
+        git rev-parse --verify HEAD
+        git merge $pr_hash || CANNOT_MERGE=1
         PR_REF=$pr_hash
       popd
+      if [[ $CANNOT_MERGE == 1 ]]; then
+        # We do not want to kill the system is github is not working
+        # so we ignore the result code for now
+        ali-bot/set-github-status -c ${PR_REPO:-alisw/alidist}@${PR_REF:-$ALIDIST_REF} -s build/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/error -m "Cannot merge PR into test area" || true
+        continue
+      fi
     fi
 
     alibuild/aliDoctor ${ALIBUILD_DEFAULTS:+--defaults $ALIBUILD_DEFAULTS} $PACKAGE || DOCTOR_ERROR=$?
     STATUS_REF=${PR_REPO:-alisw/alidist}@${PR_REF:-$ALIDIST_REF}
     if [[ $DOCTOR_ERROR != '' ]]; then
-      ali-bot/set-github-status -c ${STATUS_REF} -s doctor/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/error
+      # We do not want to kill the system is github is not working
+      # so we ignore the result code for now
+      ali-bot/set-github-status -c ${STATUS_REF} -s doctor/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/error || true
     else
-      ali-bot/set-github-status -c ${STATUS_REF} -s doctor/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/success
+      # We do not want to kill the system is github is not working
+      # so we ignore the result code for now
+      ali-bot/set-github-status -c ${STATUS_REF} -s doctor/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/success || true
     fi
-    alibuild/aliBuild -j ${JOBS:-`nproc`}                                     \
+    # Each round we delete the "latest" symlink, to avoid reporting errors
+    # from a previous one. In any case they will be recreated if needed when
+    # we build.
+    mkdir -p sw/BUILD
+    find sw/BUILD/ -maxdepth 1 -name "*latest*" -delete
+
+    GITHUB_TOKEN= alibuild/aliBuild -j ${JOBS:-`nproc`}                       \
                          ${ALIBUILD_DEFAULTS:+--defaults $ALIBUILD_DEFAULTS}  \
+                         -z $(echo ${pr_number} | tr - _)                     \
                          --reference-sources $MIRROR                          \
                          ${REMOTE_STORE:+--remote-store $REMOTE_STORE}        \
+                         ${DEBUG:+--debug}                                    \
                          build $PACKAGE || BUILD_ERROR=$?
+    STATE_CONTEXT=build/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}
     if [[ $BUILD_ERROR != '' ]]; then
-      ali-bot/set-github-status -c ${STATUS_REF} -s build/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/error
+      # We do not want to kill the system is github is not working
+      # so we ignore the result code for now
+      ali-bot/set-github-status -c ${STATUS_REF} -s $STATE_CONTEXT/error || true
+      # We do not want to kill the system is github is not working
+      # so we ignore the result code for now
+      ali-bot/report-pr-errors --default $BUILD_SUFFIX                                              \
+                               --pr "${PR_REPO:-alisw/alidist}#${pr_id}" -s $STATE_CONTEXT || true
     else
-      ali-bot/set-github-status -c ${STATUS_REF} -s build/$PACKAGE${ALIBUILD_DEFAULTS:+/$ALIBUILD_DEFAULTS}/success
+      # We do not want to kill the system is github is not working
+      # so we ignore the result code for now
+      ali-bot/set-github-status -c ${STATUS_REF} -s $STATE_CONTEXT/success || true
     fi
   done
-  sleep ${DELAY:-10}
+  sleep ${DELAY:-600} || true
 done
