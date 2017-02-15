c5fef4a182ab8e36acf828213973c55ac3173058
Dario Berzano <dario.berzano@gmail.com>
2017-02-15 16:26:33 +0100

    Improve permissions handling in data publishing (#392)

--- publish/publish-data.sh
+++ publish/publish-data.sh
@@ -8,8 +8,8 @@ REPO=$(cvmfs_server info | grep 'Repository name' | cut -d: -f2 | xargs echo)
 SRC=lxplus.cern.ch:/eos/experiment/alice/analysis-data
 DEST=/cvmfs/$REPO/data/analysis/$(TZ=Europe/Rome date +%Y/vAN-%Y%m%d)
 [[ $DRYRUN ]] && DEST=/tmp$DEST || true
-[[ -d $DEST ]] && { echo "Published already: $DEST"; exit 0; } || true
-[[ $(TZ=Europe/Rome date +%H%M) -lt 1600 ]] && { echo "Before 4pm, doing nothing"; exit 0; } || true
+[[ -d $DEST && ! $FORCE ]] && { echo "Published already: $DEST"; exit 0; } || true
+[[ $(TZ=Europe/Rome date +%H%M) -lt 1600 && ! $FORCE ]] && { echo "Before 4pm, doing nothing"; exit 0; } || true
 for I in {0..7}; do
   ERR=0
   cvmfs_server transaction || ERR=$?
@@ -23,5 +23,6 @@ dieabort() {
 }
 mkdir -p $DEST && [[ -d $DEST ]] || dieabort
 rsync -av --delete --rsh="sshpass -p `cat $HOME/.eossynccreds|cut -d: -f2-` ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -l `cat $HOME/.eossynccreds|cut -d: -f1`" $SRC/ $DEST/ || dieabort
+chmod -R u=rwX,g=rX,o=rX $DEST/
 cvmfs_server publish || dieabort
 echo "All OK"
