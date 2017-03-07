50f459e23c79f4d4135c6768c3ef74eddaa96ee8
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-03-07 15:50:29 +0100

    Add GithubCachedClient
    Barebone Github API client which has the added advantage of allowing for
    caching policies so that it can profit from the 304 responses.

--- alibot_helpers/github_utilities.py
+++ alibot_helpers/github_utilities.py
@@ -1,11 +1,130 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from collections import OrderedDict
+import requests
 from hashlib import sha1
 import pickle
 
 import sys
 import re
+
+def generateCacheId(entries):
+  h = sha1()
+  for k,v in entries:
+    h.update(k)
+    h.update(str(v))
+  return h.hexdigest()
+
+# Parses the Link header string and gets the url for the next page. If
+# the next page is not found, returns None
+def parseLinks(linkString):
+  if not linkString:
+    return None
+  links = linkString.split(",")
+  for x in links:
+    url, what = x.split(";")
+    if what.strip().startswith("rel=\"next\""):
+      sanitized = url.strip().strip("<>")
+      return sanitized
+  return None
+
+def pagination(cache_item, nextLink, api, self, stable_api):
+  for x in cache_item["payload"]:
+    yield x
+  if nextLink:
+    for x in self.request("GET", nextLink.replace(api,""), stable_api):
+      yield x
+
+class GithubCachedClient(object):
+  def __init__(self, token, api="https://api.github.com"):
+    self.token = token
+    self.api = api
+    self.cache = OrderedDict()
+
+  def loadCache(self, filename):
+    message = ""
+    try:
+      with open(filename, "r+") as f:
+        self.cache = pickle.load(f)
+        return
+    except IOError as e:
+      pass
+    except EOFError as e:
+      message = "Malformed cache file"
+    except pickle.PickleError as e:
+      message = "Could not read commit cache"
+    message and print(message, file=sys.stderr)
+    self.cache = OrderedDict()
+    return
+
+  def dumpCache(self, filename, limit=1000):
+    message = ""
+    try:
+      with open(filename, "w") as f:
+        pickle.dump(OrderedDict(self.cache.items()[-limit:]), f, 2)
+    except IOError as e:
+      message = "Unable to write cache file %s" % filename
+    except EOFError as e:
+      message = "Malformed cache file %s" % filename
+    except pickle.PickleError as e:
+      message = "Could not write to cache %s" % filename
+    print(message, file=sys.stderr)
+
+  def request(self, method, url, stable_api=True, **kwds):
+    #if not "&page=" in url:
+    #  url += "&page=1"
+    # If we have a cache getter we use it to obtain an
+    # entry in the cached etags
+    cacheHash = generateCacheId([("url", url)] + kwds.items())
+    cache_item = self.cache.get(cacheHash)
+
+    headers = {"Accept": "application/vnd.github.v3+json" if stable_api else "application/vnd.github.korra-preview",
+               "Authorization": "token %s" % self.token}
+    if cache_item and cache_item.get("ETag"):
+      headers.update({"If-None-Match": cache_item["ETag"]})
+
+    if cache_item and cache_item.get("Last-Modified"):
+      headers.update({"If-Modified-Since": cache_item["Last-Modified"]})
+    final_url = "{s.api}{url}".format(s=self, url=url).format(**kwds)
+    r = requests.request(method=method, url=final_url, headers=headers)
+    if r.status_code == 304:
+      if type(cache_item["payload"]) == list:
+        nextLink = parseLinks(cache_item.get("Link"))
+        return pagination(cache_item, nextLink, self.api, self, stable_api)
+      return cache_item["payload"]
+    # If we are here, it means we had some sort of cache miss. Therefore
+    # we pop the cacheHash from the cache.
+    try:
+      del self.cache[cacheHash]
+    except:
+      pass
+    if r.status_code == 404:
+      return None
+    if r.status_code == 403:
+      print("Forbidden", file=sys.stderr)
+      return None
+    if r.status_code == 200:
+      cache_item = {
+                    "payload": r.json(),
+                    "ETag": r.headers.get("ETag"),
+                    "Last-Modified": r.headers.get("Last-Modified"),
+                    "Link": r.headers.get("Link")
+                  }
+      self.cache.update({cacheHash: cache_item})
+      if type(cache_item["payload"]) == list:
+        nextLink = parseLinks(cache_item["Link"])
+        return pagination(cache_item, nextLink, self.api, self, stable_api)
+      return cache_item["payload"]
+    if r.status_code == 204:
+      cache_item = {
+                    "payload": True,
+                    "ETag": r.headers.get("ETag"),
+                    "Last-Modified": r.headers.get("Last-Modified")
+                  }
+      self.cache.update({cacheHash: cache_item})
+      return cache_item["payload"]
+    print(r.status_code)
+    assert(False)
 
 def printStats(gh):
   print("Github API used %s/%s" % gh.rate_limiting, file=sys.stderr)
