0c50eb45abdfbcbea6a15008d2715b7eaa86d292
Dario Berzano <dario.berzano@cern.ch>
2017-05-31 15:37:59 +0200

    Shuttle: use more generic "fetch all" from Git

--- make-shuttle.sh
+++ make-shuttle.sh
@@ -10,7 +10,7 @@ if [[ $1 != master && ! $MAKE_SHUTTLE_RETRY ]]; then
   [[ -d AliRoot_master ]] || git clone --reference sw/MIRROR/aliroot https://github.com/alisw/AliRoot AliRoot_master/
   pushd AliRoot_master
     # Upstream master of AliRoot
-    git fetch --all --tags
+    git remote update -p && git fetch && git fetch --tags
     git clean -fxd
     git checkout master
     git reset --hard origin/master
@@ -18,7 +18,7 @@ if [[ $1 != master && ! $MAKE_SHUTTLE_RETRY ]]; then
 fi
 pushd AliRoot
   # Upstream user-defined tag of AliRoot
-  git fetch --all --tags
+  git remote update -p && git fetch && git fetch --tags
   git clean -fxd
   git checkout master
   git reset --hard refs/tags/$1 || git reset --hard origin/$1 || git reset --hard $1
