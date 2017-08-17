a63b7ab2bb9e11b078caa67e4efbc807cde4f0a5
Dario Berzano <dario.berzano@cern.ch>
2017-08-17 14:21:38 +0200

    Use pip to get aliBuild and dependencies installed

--- build-any-ib.sh
+++ build-any-ib.sh
@@ -15,7 +15,13 @@ MIRROR=/build/mirror
 WORKAREA=${WORKAREA:-/build/workarea/sw/$BUILD_DATE}
 WORKAREA_INDEX=0
 
-git clone -b $ALIBUILD_BRANCH https://github.com/$ALIBUILD_REPO/alibuild
+# Get aliBuild with pip in a temporary directory. Gets all dependencies too
+export PYTHONUSERBASE=$(mktemp -d)
+export PATH=$PYTHONUSERBASE/bin:$PATH
+export LD_LIBRARY_PATH=$PYTHONUSERBASE/lib:$LD_LIBRARY_PATH
+pip install --user git+https://github.com/$ALIBUILD_REPO/alibuild${ALIBUILD_BRANCH:+@$ALIBUILD_BRANCH}
+type aliBuild
+
 if [[ $ALIDIST_BRANCH =~ pull/ ]]; then
   git clone https://github.com/$ALIDIST_REPO/alidist
   pushd alidist
@@ -51,7 +57,7 @@ done
 
 # Allow to specify AliRoot and AliPhysics as a development packages
 if [[ "$ALIROOT_DEVEL_VERSION" != '' ]]; then
-  alibuild/aliBuild init AliRoot@$ALIROOT_DEVEL_VERSION
+  aliBuild init AliRoot@$ALIROOT_DEVEL_VERSION
   pushd AliRoot
     # Either pull changes in the branch or reset it to the requested tag
     git pull origin || git reset --hard $ALIROOT_DEVEL_VERSION
@@ -60,7 +66,7 @@ else
   rm -rf AliRoot
 fi
 if [[ "$ALIPHYSICS_DEVEL_VERSION" != '' ]]; then
-  alibuild/aliBuild init AliPhysics@$ALIPHYSICS_DEVEL_VERSION
+  aliBuild init AliPhysics@$ALIPHYSICS_DEVEL_VERSION
   pushd AliPhysics
     # Either pull changes in the branch or reset it to the requested tag
     git pull origin || git reset --hard $ALIPHYSICS_DEVEL_VERSION
@@ -73,15 +79,15 @@ RWOPT='::rw'
 [[ "$PUBLISH_BUILDS" == "false" ]] && RWOPT=
 REMOTE_STORE="${REMOTE_STORE:-rsync://repo.marathon.mesos/store/}$RWOPT"
 [[ "$USE_REMOTE_STORE" == "false" ]] && REMOTE_STORE=
-alibuild/aliBuild --reference-sources $MIRROR                    \
-                  --debug                                        \
-                  --work-dir $WORKAREA/$WORKAREA_INDEX           \
-                  --architecture $ARCHITECTURE                   \
-                  --jobs 16                                      \
-                  ${REMOTE_STORE:+--remote-store $REMOTE_STORE}  \
-                  ${DEFAULTS:+--defaults $DEFAULTS}              \
-                  ${DISABLE:+--disable $DISABLE}                 \
-                  build $PACKAGE_NAME || BUILDERR=$?
+aliBuild --reference-sources $MIRROR                    \
+         --debug                                        \
+         --work-dir $WORKAREA/$WORKAREA_INDEX           \
+         --architecture $ARCHITECTURE                   \
+         --jobs 16                                      \
+         ${REMOTE_STORE:+--remote-store $REMOTE_STORE}  \
+         ${DEFAULTS:+--defaults $DEFAULTS}              \
+         ${DISABLE:+--disable $DISABLE}                 \
+         build $PACKAGE_NAME || BUILDERR=$?
 
 rm -f $WORKAREA/$WORKAREA_INDEX/current_slave
 [[ "$BUILDERR" != '' ]] && exit $BUILDERR
@@ -90,7 +96,8 @@ echo ALIROOT_BUILD_NR=$BUILD_NUMBER >> results.props
 echo PACKAGE_NAME=$PACKAGE_NAME >> results.props
 
 ALIDIST_HASH=$(cd $WORKSPACE/alidist && git rev-parse HEAD)
-ALIBUILD_HASH=$(cd $WORKSPACE/alibuild && git rev-parse HEAD)
+ALIBUILD_HASH=$(aliBuild version 2> /dev/null || true)
+rm -rf $PYTHONUSERBASE
 
 case $PACKAGE_NAME in
   aliroot*|zlib*)
