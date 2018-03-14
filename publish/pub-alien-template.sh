b939768ca76d3b1fc35c0367d5e10e07728e8e7b
Dario Berzano <dario.berzano@cern.ch>
2018-03-14 01:46:05 +0100

    Do not publish AliEn tarballs for real
    * Create dummy tarballs (almost-empty) to keep Packman happy
    * Do not trigger torrent mirroring any longer

--- publish/pub-alien-template.sh
+++ publish/pub-alien-template.sh
@@ -6,19 +6,16 @@ CONNRETRY=%(conn_retries)d
 CONNRETRYDELAY=%(conn_dethrottle_s)d
 [[ $SSLVERIFY == 0 ]] && SSLVERIFY=-k || SSLVERIFY=
 TMPDIR=$(mktemp -d /tmp/aliPublish.XXXXX)
-TORDIR=/var/packages/download
-TORNOTIFY=/var/packages/NEWFILE
 mkdir -p "$TMPDIR"
 cd "$TMPDIR"
-curl -Lsf $SSLVERIFY                \
-     --connect-timeout $CONNTIMEOUT \
-     --retry-delay $CONNRETRYDELAY  \
-     --retry $CONNRETRY "%(url)s"   | tar --strip-components=3 -xzf -
+
+# Create dummy tarball to make Packman happy
 TAR="$(echo "%(package)s"|tr '[:upper:]' '[:lower:]')_%(version)s.%(arch)s.tar.gz"
-[[ -e "%(version)s/etc/modulefiles/%(package)s" ]]
+mkdir "%(version)s"
+echo "$(package)s %(version)s %(arch)s" > "%(version)s/package.txt" 
 tar czf "$TAR" "%(version)s/"
 rm -rf "%(version)s/"
-DEPS="%(dependencies)s"
+
 BEST_SES=$(curl -sL 'http://alimonitor.cern.ch/services/getBestSE.jsp?count=4&op=0' | \
            grep -E '^[^: ]+::[^: ]+::[^: ]+$' | sort -R)
 [[ "$BEST_SES" != '' ]] || BEST_SES="ALICE::CERN::EOS"
@@ -28,15 +25,9 @@ for SE in $BEST_SES; do
   echo "Trying SE $SE"
   alien -exec packman define "%(package)s" "%(version)s" \
               "$TMPDIR/$TAR"                             \
-              ${DEPS:+dependencies=$DEPS}                \
               -vo -platform %(arch)s -se $SE || continue
   ERR=0
 done
 [[ $ERR == 1 ]] && { echo "All storage elements failed"; exit 1; } || true
-cp -f "$TMPDIR/$TAR" "$TORDIR/$TAR"
-chmod a=rw "$TORDIR/$TAR"
-touch "$TORNOTIFY"
-alien -exec addMirror \
-            "/alice/packages/%(package)s/%(version)s/%(arch)s" \
-            no_se "torrent://alitorrent.cern.ch/torrents/$TAR.torrent"
+cd /
 rm -rf "$TMPDIR"
