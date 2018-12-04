ba904bf10c7b51d5feba2fb48429625ba7a2359b
Dario Berzano <dario.berzano@gmail.com>
2018-12-04 16:39:06 +0100

    Set CI environment to UTF-8

--- ci/continuous-builder.sh
+++ ci/continuous-builder.sh
@@ -104,6 +104,23 @@ function emptylog() {
   rm -rf copy-emptylog
 }
 
+# Explicitly set UTF-8 support (Python needs it!)
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_PAPER="en_US.UTF-8"
+export LC_NAME="en_US.UTF-8"
+export LC_ADDRESS="en_US.UTF-8"
+export LC_TELEPHONE="en_US.UTF-8"
+export LC_MEASUREMENT="en_US.UTF-8"
+export LC_IDENTIFICATION="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 report_state started
 
 while true; do
