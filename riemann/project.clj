0108504536973fd1de9fd471390403ca10b13205
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-06-25 00:24:53 +0200

    Drop riemann reports from aliPublish (#772)
    Simply a bad idea. Cleaning up.

--- riemann/project.clj
+++
@@ -1,8 +0,0 @@
-; A lein project to install dependencies.
-(defproject alice-riemann-config "0.1.0-SNAPSHOT"
-  :description "Monalisa Configuration for riemann"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-time/clj-time "0.11.0"]
-                 [clj-http/clj-http "2.0.1"]
-                ]
-)
