3f624d87eb1725bf7b6f7167840943c44018b55b
Timo Wilken <timo.wilken@cern.ch>
2020-10-16 14:49:48 +0200

    Loop through repos
    This is the heart of the PR. Here, we loop through all repositories that are
    compatible with the docker image we are running under. The definitions for that
    are stored in ci/repo-config/*/*.env.

--- ci/continuous-builder.sh
+++ ci/continuous-builder.sh
@@ -11,7 +11,17 @@ function long_timeout () { $TIMEOUT_EXEC -s9 "$(get_config_value long-timeout "$
 
 . build-helpers.sh
 
+# In one-repo-only mode, environment definition files aren't read, and we assume
+# e.g. aurora has already given us the required environment for the one specific
+# build we should do.
+ONE_REPO_ONLY=
+for arg in "$@"; do
+  if [ "$arg" = --one-repo-only ]; then
+    ONE_REPO_ONLY=true
+  fi
+done
 
+git clone https://github.com/alisw/ali-bot
 
 # Set up common global environment
 # Mesos DNSes
@@ -20,16 +30,58 @@ export MESOS_DNS
 # Explicitly set UTF-8 support (Python needs it!)
 export {LANG{,UAGE},LC_{CTYPE,NUMERIC,TIME,COLLATE,MONETARY,PAPER,MESSAGES,NAME,ADDRESS,TELEPHONE,MEASUREMENT,IDENTIFICATION,ALL}}=en_US.UTF-8
 
-report_state started
 # GitLab credentials for private ALICE repositories
 printf 'protocol=https\nhost=gitlab.cern.ch\nusername=%s\npassword=%s\n' "$GITLAB_USER" "$GITLAB_PASS" |
   git credential-store --file ~/.git-creds store
 git config --global credential.helper 'store --file ~/.git-creds'
 
+report_state started
 
 while true; do
-  source `which build-helpers.sh || echo ci/build-helpers.sh`
-  # We source the actual build loop, so that whenever we repeat it,
-  # we can get a new version.
-  source `which build-loop.sh || echo ci/build-loop.sh`
+  if [ -n "$ONE_REPO_ONLY" ]; then
+    # All the environment variables we need are already set by aurora, so just
+    # run the build.
+    (. build-loop.sh)
+  else
+    cur_container=${CONTAINER_IMAGE#*/}
+    cur_container=${cur_container%-builder:*}
+    # If there is no repo we can build in this container, wait; maybe something
+    # will turn up.
+    if ! [ -d "ali-bot/ci/repo-config/$cur_container" ]; then
+      sleep 600
+      continue
+    fi
+
+    # Loop through repositories we can build (i.e. that need the docker
+    # container we are in).
+    for env_file in "ali-bot/ci/repo-config/$cur_container"/*.env; do
+      # Run iterations in a subshell so environment variables are not kept
+      # across potentially different repos. This is an issue as env files are
+      # allowed to define arbitrary variables that other files (or the defaults
+      # file) might not override.
+      (
+        # Set up environment
+        . "ali-bot/ci/repo-config/defaults.env" || exit
+        . "$env_file"                           || exit   # in case there are no env files
+
+        # Make a directory for this repo's dependencies so they don't conflict
+        # with other repos'
+        repo=${env_file#repo-config/$cur_container/}
+        repo=${repo%.env}
+        mkdir -p "$repo"
+        cd "$repo" || exit 10
+
+        # Get dependency development packages
+        echo "$DEVEL_PKGS" | while read -r gh_url branch checkout_name; do
+          git clone "https://github.com/$gh_url" ${branch:+--branch "$branch"} ${checkout_name:+"$checkout_name"}
+        done
+
+        # Run the build
+        . build-loop.sh
+      )
+    done
+  fi
+
+  # Get updates to ali-bot
+  reset_git_repository ali-bot
 done
