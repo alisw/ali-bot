f8fab8d0270c54a798ad62f9e28dc9764f33ce4b
Timo Wilken <timo.wilken@cern.ch>
2020-11-10 16:20:15 +0100

    Remove run-continuous-builder.sh wrapper
    The wrapper mostly does the same setup as continuous-builder.sh; what it did
    extra before is now incorporated into continuous-builder.sh as well.
    
    MacOS builds are now handled by continuous-builder.sh.
    
    A few more variables need to be initialised outside the script on MacOS -- see
    https://alice.its.cern.ch/jira/browse/O2-1817.

--- ci/continuous-builder.sh
+++ ci/continuous-builder.sh
@@ -3,38 +3,71 @@
 # AliRoot / AliPhysics. Notice this will do an incremental build, not a full
 # build, so it really to catch errors earlier.
 
-# timeout vs. gtimeout (macOS with Homebrew)
-TIMEOUT_EXEC=timeout
-type $TIMEOUT_EXEC > /dev/null 2>&1 || TIMEOUT_EXEC=gtimeout
-function short_timeout () { $TIMEOUT_EXEC -s9 "$TIMEOUT" "$@"; }
-function long_timeout () { $TIMEOUT_EXEC -s9 "$LONG_TIMEOUT" "$@"; }
-
 . build-helpers.sh
 
-if ! [ -d ali-bot ]; then
-  git clone https://github.com/alisw/ali-bot
-fi
+if [ "$1" != --skip-setup ]; then
+  for var in GITHUB_TOKEN GITLAB_USER GITLAB_PASS AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY INFLUXDB_WRITE_URL ALIBOT_ANALYTICS_ID MONALISA_HOST MONALISA_PORT MESOS_ROLE CONTAINER_IMAGE; do
+    if [ -z "${!var}" ]; then
+      echo "$(basename "$0"): error: required variable $V not defined!" >&2
+      exit 1
+    else
+      export "${var?}"
+    fi
+  done
 
-# Set up common global environment
-# Mesos DNSes
-: "${MESOS_DNS:=alimesos01.cern.ch,alimesos02.cern.ch,alimesos03.cern.ch}"
-export MESOS_DNS
-# Explicitly set UTF-8 support (Python needs it!)
-export {LANG{,UAGE},LC_{CTYPE,NUMERIC,TIME,COLLATE,MONETARY,PAPER,MESSAGES,NAME,ADDRESS,TELEPHONE,MEASUREMENT,IDENTIFICATION,ALL}}=en_US.UTF-8
+  case "$(uname)" in
+    # The MacOS builders aren't in the right directory at first, apparently.
+    Darwin) ci_work_dir=/build/ci_checks/alice_ci_$WORKER_INDEX
+            mkdir -p "$ci_work_dir"
+            cd "$ci_work_dir" || exit 1;;
+  esac
 
-# GitLab credentials for private ALICE repositories
-printf 'protocol=https\nhost=gitlab.cern.ch\nusername=%s\npassword=%s\n' "$GITLAB_USER" "$GITLAB_PASS" |
-  git credential-store --file ~/.git-creds store
-git config --global credential.helper 'store --file ~/.git-creds'
+  # timeout vs. gtimeout (macOS with Homebrew)
+  TIMEOUT_EXEC=timeout
+  type $TIMEOUT_EXEC > /dev/null 2>&1 || TIMEOUT_EXEC=gtimeout
+  function short_timeout () { $TIMEOUT_EXEC -s9 "$TIMEOUT" "$@"; }
+  function long_timeout () { $TIMEOUT_EXEC -s9 "$LONG_TIMEOUT" "$@"; }
+  export -f short_timeout long_timeout
 
-# This turns a container image (e.g. alisw/slc8-gpu-builder:latest) into a
-# short, simple name like slc8-gpu that we use for the .env directories.
-CUR_CONTAINER=${CONTAINER_IMAGE#*/}
-export CUR_CONTAINER=${CUR_CONTAINER%-builder:*}
+  if ! [ -d ali-bot ]; then
+    # This is for *.env files. These should always be taken from ali-bot@master,
+    # irrespective of the *installed* ali-bot version required by each repo.
+    git clone https://github.com/alisw/ali-bot
+  fi
 
-# Generate example of force-hashes file. This is used to override what to check for testing
-if ! [ -e force-hashes ]; then
-  cat > force-hashes <<EOF
+  # Disable aliBuild analytics prompt
+  mkdir -p ~/.config/alibuild
+  touch ~/.config/alibuild/disable-analytics
+
+  # Set up common global environment
+  # Get the number of processors on this system, in case JOBS= isn't given.
+  JOBS_DEFAULT=$(nproc 2>/dev/null ||
+                   sysctl -n hw.ncpu 2>/dev/null ||
+                   grep -c bogomips /proc/cpuinfo 2>/dev/null ||
+                   echo 4)
+  # Mesos DNSes
+  : "${MESOS_DNS:=alimesos01.cern.ch,alimesos02.cern.ch,alimesos03.cern.ch}"
+  # Explicitly set UTF-8 support (Python needs it!)
+  export {LANG{,UAGE},LC_{CTYPE,NUMERIC,TIME,COLLATE,MONETARY,PAPER,MESSAGES,NAME,ADDRESS,TELEPHONE,MEASUREMENT,IDENTIFICATION,ALL}}=en_US.UTF-8
+  export JOBS_DEFAULT MESOS_DNS
+
+  # GitLab credentials for private ALICE repositories
+  printf 'protocol=https\nhost=gitlab.cern.ch\nusername=%s\npassword=%s\n' "$GITLAB_USER" "$GITLAB_PASS" |
+    git credential-store --file ~/.git-creds store
+  git config --global credential.helper 'store --file ~/.git-creds'
+
+  # Git user setup
+  git config --global user.name alibuild
+  git config --global user.email alibuild@cern.ch
+
+  # This turns a container image (e.g. alisw/slc8-gpu-builder:latest) into a
+  # short, simple name like slc8-gpu that we use for the .env directories.
+  CUR_CONTAINER=${CONTAINER_IMAGE#*/}
+  export CUR_CONTAINER=${CUR_CONTAINER%-builder:*}
+
+  # Generate example of force-hashes file. This is used to override what to check for testing
+  if ! [ -e force-hashes ]; then
+    cat > force-hashes <<EOF
 # Override what to build using this file.
 # Lines are of the form:
 # BUILD_TYPE (PR_NUMBER|BRANCH_NAME) PR_HASH ENV_NAME
@@ -42,6 +75,7 @@ if ! [ -e force-hashes ]; then
 # - BUILD_TYPE: one of: not_tested, not_successful, tested, reviewed
 # - ENV_NAME: the basename without ".env" of the .env file to source
 EOF
+  fi
 fi
 
 # Get a list of PRs to build -- force-hashes overrides list-branch-pr.
@@ -68,10 +102,22 @@ if [ -n "$HASHES" ]; then
     mkdir -p "$env_name"
     cd "$env_name" || exit 10
 
+    # Use pip2 --user if required.
+    if [ -n "$VIRTUAL_ENV" ] || [ "$(whoami)" = root ]; then
+      pip_user=
+    else
+      # Use repo-specific PYTHONUSERBASEs if we're installing for this user.
+      pip_user=--user
+      export PYTHONUSERBASE=$PWD/python_local
+      export PATH=$PYTHONUSERBASE/bin${PATH+:$PATH}
+      export LD_LIBRARY_PATH=$PYTHONUSERBASE/lib${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}
+      mkdir -p "$PYTHONUSERBASE"
+    fi
+
     # Sometimes pip gets stuck when cloning the ali-bot or alibuild repos. In
     # that case: time out, skip and try again later.
-    short_timeout pip2 install --upgrade --upgrade-strategy only-if-needed "git+https://github.com/$INSTALL_ALIBOT"   || exit
-    short_timeout pip2 install --upgrade --upgrade-strategy only-if-needed "git+https://github.com/$INSTALL_ALIBUILD" || exit
+    short_timeout pip2 install $pip_user --upgrade --upgrade-strategy only-if-needed "git+https://github.com/$INSTALL_ALIBOT"   || exit
+    short_timeout pip2 install $pip_user --upgrade --upgrade-strategy only-if-needed "git+https://github.com/$INSTALL_ALIBUILD" || exit
 
     # Run the build
     . build-loop.sh
@@ -84,4 +130,10 @@ fi
 # Get updates to ali-bot
 TIMEOUT=$(get_config_value timeout "${TIMEOUT:-600}") reset_git_repository ali-bot
 
-exec continuous-builder.sh
+# Run CI builder under screen if possible and we're not already there. This is
+# for the macOS builders.
+if [ -n "$STY" ] || ! type screen > /dev/null; then
+  exec "$0" --skip-setup "$@"
+else
+  exec screen -dmS "ci_$WORKER_INDEX" "$0" --skip-setup "$@"
+fi
