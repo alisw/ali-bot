82f80e10a459cae622d554ef21384967381198f6
Timo Wilken <timo.wilken@cern.ch>
2021-02-10 12:29:51 +0100

    Loop inside the script, to simplify aurora job definition

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# Check if variables are provided.
-[ X${DEBUG:-1} = X1 ] && set -x
+yum install -y rclone
+pip install -U boto3
 
-for CONF in aliPublish*-rpms.conf; do
-  echo === $(LANG=C date) :: running for configuration $CONF === >&2
-  ./aliPublish --config "$CONF" --debug sync-rpms >&2
-  timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/RPMS/el7.x86_64/ rpms3:alibuild-repo/RPMS/el7.x86_64/ || true
-  timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/UpdRPMS/el7.x86_64/ rpms3:alibuild-repo/UpdRPMS/el7.x86_64/ || true
-  printf "\n\n\n\n" >&2
+. /secrets/alibot_secrets
+. /etc/profile.d/enable-alice.sh
+export FLP_RPM_CI_TOKEN SYNC_PASS=$ALIBOT_PASSWORD SYNC_USER=alibot
+
+cd "$(dirname "$0")" || exit 2
+while true; do
+  # Supply possible conf suffix, e.g. -cc8, as arg to this script.
+  for conf in aliPublish*-rpms"$1".conf; do
+    echo "=== $(LANG=C date) :: running for configuration $conf ===" >&2
+    ./aliPublish --config "$conf" --debug sync-rpms >&2 || exit 1
+    for rpmtype in RPMS UpdRPMS; do
+      for arch in el7.x86_64 el8.x86_64; do
+        timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose \
+                "local:/repo/$rpmtype/$arch/" "rpms3:alibuild-repo/$rpmtype/$arch/"
+      done
+    done
+    printf '\n\n\n\n' >&2
+  done
+  sleep 600
 done
