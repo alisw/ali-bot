5c6dff70aa5e9afd87c2881ed8a1d29ccc8c5912
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-02-10 14:13:06 +0100

    Only run non updatable RPMS

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -3,10 +3,5 @@
 # Check if variables are provided.
 [ X${DEBUG:-1} = X1 ] && set -x
 
-for CONF in aliPublish*-rpms.conf; do
-  echo === $(LANG=C date) :: running for configuration $CONF === >&2
-  ./aliPublish --config "$CONF" --debug --cache-deps-dir /tmp/pubdepscache sync-rpms >&2
-  timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/RPMS/el7.x86_64/ rpms3:alibuild-repo/RPMS/el7.x86_64/ || true
-  timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/UpdRPMS/el7.x86_64/ rpms3:alibuild-repo/UpdRPMS/el7.x86_64/ || true
-  printf "\n\n\n\n" >&2
-done
+./aliPublish --config "aliPublish-rpms.conf" --debug --cache-deps-dir /tmp/pubdepscache sync-rpms >&2
+timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/RPMS/el7.x86_64/ rpms3:alibuild-repo/RPMS/el7.x86_64/ || true
