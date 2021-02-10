b9b31b537b404b092ea8dee1a4a81ff5683331f1
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-02-10 10:10:25 +0100

    Add separate template for updatable RPMS

---
+++ publish/pub-updatable-rpms-template.sh
@@ -0,0 +1,177 @@
+#!/bin/bash -ex
+set -o pipefail
+INSTALLPREFIX=/opt/alisw
+TMPDIR="%(workdir)s"
+FULLVER="%(version)s"
+VERSION=${FULLVER%%-*}
+REVISION=${FULLVER##*-}
+FULLARCH="%(arch)s"
+FLAVOUR=${FULLARCH%%.*}
+ARCHITECTURE=${FULLARCH##*.}
+SSLVERIFY=%(http_ssl_verify)d
+CONNTIMEOUT=%(conn_timeout_s)d
+CONNRETRY=%(conn_retries)d
+CONNRETRYDELAY=%(conn_dethrottle_s)d
+RPM_IS_UPDATABLE=%(updatable)s
+[[ $SSLVERIFY == 0 ]] && SSLVERIFY=-k || SSLVERIFY=
+which fpm
+cd $TMPDIR
+
+if [[ $RPM_IS_UPDATABLE ]]; then
+  case "%(dependencies)s" in
+    *AliEn-runtime*) ;;
+    *) echo "Not publishing %(package)s with version %(version)s as it has AliEn-Runtime as a dependency";  exit 0 ;;
+  esac
+fi
+
+# Create aliswmod RPM
+ALISWMOD_VERSION=4
+ALISWMOD_RPM="alisw-aliswmod-$ALISWMOD_VERSION-1.%(arch)s.rpm"
+if [[ ! -e "%(repodir)s/$ALISWMOD_RPM" ]]; then
+  mkdir -p aliswmod/bin
+  mkdir -p aliswmod/etc/profile.d
+  cat > aliswmod/etc/profile.d/99-aliswmod.sh << EOF
+export LD_LIBRARY_PATH=/opt/alisw/el7/lib:/opt/alisw/el7/lib64:\$LD_LIBRARY_PATH
+export PATH=/opt/alisw/el7/bin:\$PATH
+export MODULEPATH=$INSTALLPREFIX/$FLAVOUR/modulefiles:$INSTALLPREFIX/$FLAVOUR/etc/Modules/modulefiles:\$MODULEPATH
+EOF
+  pushd aliswmod
+    fpm -s dir                                 \
+        -t rpm                                 \
+        --force                                \
+        --depends 'environment-modules >= 4.0' \
+        --prefix /                             \
+        --architecture $ARCHITECTURE           \
+        --version $ALISWMOD_VERSION            \
+        --iteration 1.$FLAVOUR                 \
+        --name alisw-aliswmod                  \
+        .
+  popd
+  mv aliswmod/$ALISWMOD_RPM .
+  rm -rf aliswmod/
+else
+  echo No need to create the package, skipping
+  ALISWMOD_RPM=
+fi
+
+DEPS=()
+DEPS+=("--depends" "alisw-aliswmod >= $ALISWMOD_VERSION")
+
+# Updatable RPMs don't have the version number hardcoded in the package name
+if [[ $RPM_IS_UPDATABLE ]]; then
+  RPM_VERSION="%(version)s"
+  RPM_VERSION=${RPM_VERSION//-/_}
+  RPM_PACKAGE="alisw-%(package)s"
+  RPM_TAR_STRIP=4
+  RPM_ROOT="."
+  RPM_UNPACK_DIR="."
+  RPM_MODULEFILE_PREFIX=
+  for D in %(dependencies)s; do
+    DEP_NAME=${D%%+*}
+    DEP_VER=${D#*+}
+    DEP_VER=${DEP_VER//-/_}
+    DEPS+=("--depends" "$DEP_NAME >= ${DEP_VER}-1.$FLAVOUR")
+  done
+else
+  RPM_VERSION=1
+  RPM_PACKAGE="alisw-%(package)s+%(version)s"
+  RPM_TAR_STRIP=2
+  RPM_ROOT="%(package)s/%(version)s"
+  RPM_UNPACK_DIR="%(package)s"
+  for D in %(dependencies)s; do
+    DEPS+=("--depends" "$D = 1-1.$FLAVOUR")
+  done
+fi
+
+# Create RPM from tarball
+mkdir -p unpack_rpm
+pushd unpack_rpm
+  # RPM's root dir will be $TMPDIR/unpack_rpm/$RPM_UNPACK_DIR
+  curl -Lsf $SSLVERIFY                \
+       --connect-timeout $CONNTIMEOUT \
+       --retry-delay $CONNRETRYDELAY  \
+       --retry $CONNRETRY "%(url)s"   | tar --strip-components=$RPM_TAR_STRIP -xzf -
+  # Rename "bad" files -- see https://github.com/jordansissel/fpm/issues/1385
+  while read BAD_FILE; do
+    mv -v "$BAD_FILE" "${BAD_FILE//\'/_}"
+  done < <(find . -name "*'*" || true)
+  # Add extra dependencies, if applicable
+  if [[ -e $RPM_ROOT/.rpm-extra-deps ]]; then
+    OLD_IFS="$IFS"
+    IFS=$'\n'
+    for D in $(cat "$RPM_ROOT/.rpm-extra-deps" | sed -e 's/[ ]*#.*//;s/ *\(.*\) */\1/g; /^$/d'); do
+      DEPS+=("--depends" "$D")
+    done
+    IFS="$OLD_IFS"
+  fi
+  # Remove useless files conflicting between packages
+  if [ ! "X$RPM_IS_UPDATABLE" = X ]; then
+    mv $RPM_ROOT/.build-hash $RPM_ROOT/.build-hash.%(package)s 
+    mv $RPM_ROOT/.rpm-extra-deps $RPM_ROOT/.rpm-extra-deps.%(package)s
+    mv $RPM_ROOT/.original-unrelocated $RPM_ROOT/.original-unrelocated.%(package)s 
+    mv $RPM_ROOT/etc/profile.d/init.sh $RPM_ROOT/etc/profile.d/init.sh.%(package)s  
+  fi
+popd
+
+AFTER_INSTALL=$TMPDIR/after_install.sh
+AFTER_REMOVE=$TMPDIR/after_remove.sh
+
+cat > $AFTER_INSTALL <<EOF
+#!/bin/bash -e
+RPM_IS_UPDATABLE=$RPM_IS_UPDATABLE
+export WORK_DIR=$INSTALLPREFIX
+cd \$WORK_DIR
+[[ \$RPM_IS_UPDATABLE ]] && export PKGPATH=${FLAVOUR} || export PKGPATH="${FLAVOUR}/%(package)s/%(version)s"
+EOF
+grep -v 'profile\.d/init\.sh\.unrelocated' unpack_rpm/$RPM_ROOT/relocate-me.sh >> $AFTER_INSTALL
+rm -fv unpack_rpm/$RPM_ROOT/relocate-me.sh
+cat >> $AFTER_INSTALL <<EOF
+MODULE_DEST_DIR=$INSTALLPREFIX/$FLAVOUR/\${RPM_IS_UPDATABLE:+etc/Modules/}modulefiles
+mkdir -p \$MODULE_DEST_DIR/%(package)s
+if [[ \$RPM_IS_UPDATABLE ]]; then
+  sed -e 's|%(package)s/\$version||g; s|%(package)s/%(version)s||g' $INSTALLPREFIX/$FLAVOUR/etc/modulefiles/%(package)s > \$MODULE_DEST_DIR/%(package)s/%(version)s
+else
+  ln -nfs ../../%(package)s/%(version)s/etc/modulefiles/%(package)s \$MODULE_DEST_DIR/%(package)s/%(version)s
+fi
+mkdir -p \$MODULE_DEST_DIR/BASE
+echo -e "#%%Module\nsetenv BASEDIR $INSTALLPREFIX/$FLAVOUR" > \$MODULE_DEST_DIR/BASE/1.0
+EOF
+
+cat > $AFTER_REMOVE <<EOF
+#!/bin/bash
+( rm -f $INSTALLPREFIX/$FLAVOUR/modulefiles/%(package)s/%(version)s
+  rm -f $INSTALLPREFIX/$FLAVOUR/etc/Modules/modulefiles/%(package)s/%(version)s
+  rmdir $INSTALLPREFIX/$FLAVOUR/modulefiles/%(package)s
+  find $INSTALLPREFIX/$FLAVOUR/%(package)s/%(version)s -depth -type d -exec rmdir '{}' \;
+  rmdir $INSTALLPREFIX/$FLAVOUR/%(package)s
+  if [[ "\$(find $INSTALLPREFIX/$FLAVOUR -mindepth 1 -maxdepth 1 -type d -not -name modulefiles )" == "" ]]; then
+    rm -rf $INSTALLPREFIX/$FLAVOUR
+    rmdir $INSTALLPREFIX
+  fi
+) 2> /dev/null
+true
+EOF
+
+# We must put the full version in the package name to allow multiple versions
+# to be installed at the same time, see [1]
+# [1] http://www.rpm.org/wiki/PackagerDocs/MultipleVersions
+pushd unpack_rpm
+  fpm -s dir                           \
+      -t rpm                           \
+      --force                          \
+      "${DEPS[@]}"                     \
+      --prefix $INSTALLPREFIX/$FLAVOUR \
+      --architecture $ARCHITECTURE     \
+      --version "$RPM_VERSION"         \
+      --iteration 1.$FLAVOUR           \
+      --name "$RPM_PACKAGE"            \
+      --exclude compile_commands.json  \
+      --after-install $AFTER_INSTALL   \
+      --after-remove $AFTER_REMOVE     \
+      "$RPM_UNPACK_DIR"
+  RPM="${RPM_PACKAGE}-${RPM_VERSION}-1.%(arch)s.rpm"
+  [[ -e $RPM ]]
+  mkdir -vp %(repodir)s
+  mv -v $RPM ../
+popd
+mv -v $RPM $ALISWMOD_RPM %(stagedir)s
