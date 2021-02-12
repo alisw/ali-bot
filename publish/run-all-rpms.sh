9e2a1066e23e54e0cf3d0e2d377632eca73e3208
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-02-12 17:02:52 +0100

    Add profiling for publisher

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -3,5 +3,5 @@
 # Check if variables are provided.
 [ X${DEBUG:-1} = X1 ] && set -x
 
-./aliPublish --config "aliPublish-rpms.conf" --debug --cache-deps-dir /tmp/pubdepscache sync-rpms >&2
+python -m cProfile -s cumtime ./aliPublish --config "aliPublish-rpms.conf" --debug --cache-deps-dir /tmp/pubdepscache sync-rpms >&2
 timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/RPMS/el7.x86_64/ rpms3:alibuild-repo/RPMS/el7.x86_64/ || true
