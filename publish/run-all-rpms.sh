cd81a391e24c627e28017aa8f8f57abc122a6152
Timo Wilken <timo.wilken@cern.ch>
2021-02-16 16:10:46 +0100

    Move looping into run-all-rpms.sh script
    The new script also pulls changes to ali-bot on every iteration, and loops
    faster (sleeping only a minute between iterations).
    
    This is to simplify the aurora service definition.

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -1,7 +1,31 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-# Check if variables are provided.
-[ X${DEBUG:-1} = X1 ] && set -x
+# The architecture should correspond to the architecture built using the
+# requested config files -- only these architectures' repos will be synced.
+arch=$1
+shift
+# $@ now contains only the config files to read.
 
-python -m cProfile -s cumtime ./aliPublish --config "aliPublish-rpms.conf" --debug --cache-deps-dir /tmp/pubdepscache sync-rpms >&2
-timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/RPMS/el7.x86_64/ rpms3:alibuild-repo/RPMS/el7.x86_64/ || true
+. /etc/profile.d/enable-alice.sh
+# aliPublish needs these S3 secrets.
+# rclone gets them from /secrets/alibuild_rclone_config.
+. /secrets/aws_bot_secrets
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+
+cd "$(dirname "$0")"
+while true; do
+  # Update the ali-bot repository clone to get updated configs.
+  # Remote changes override local ones.
+  git pull --rebase --strategy=recursive --strategy-option=theirs
+
+  for conf in "$@"; do
+    ./aliPublish --config "$conf" --debug sync-rpms >&2
+  done
+
+  timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose \
+          "local:/repo/RPMS/$arch.x86_64/" "rpms3:alibuild-repo/RPMS/$arch.x86_64/" || true
+  timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose \
+          "local:/repo/UpdRPMS/$arch.x86_64/" "rpms3:alibuild-repo/UpdRPMS/$arch.x86_64/" || true
+
+  sleep 60
+done
