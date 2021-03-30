e3ee8b82caea275b06e336fad42cc15d59c26799
Timo Wilken <timo.wilken@cern.ch>
2021-03-30 08:47:50 +0000

    Fix development package fetching (#998)

--- ci/build-loop.sh
+++ ci/build-loop.sh
@@ -55,12 +55,12 @@ export ALIBOT_ANALYTICS_USER_UUID ALIBOT_ANALYTICS_ARCHITECTURE
 export ALIBOT_ANALYTICS_APP_NAME=continuous-builder.sh
 
 # Get dependency development packages
-# Use printf without a trailing newline so that the while loop doesn't run if
-# $DEVEL_PKGS is empty.
-printf %s "$DEVEL_PKGS" | while read -r gh_url branch checkout_name; do
-  reset_git_repository "${checkout_name:-$(basename "$gh_url")}" \
-                       "https://github.com/$gh_url" ${branch:+--branch "$branch"}
-done
+if [ -n "$DEVEL_PKGS" ]; then
+  echo "$DEVEL_PKGS" | while read -r gh_url branch checkout_name; do
+    reset_git_repository "${checkout_name:-$(basename "$gh_url")}" \
+                         "https://github.com/$gh_url" ${branch:+--branch "$branch"}
+  done
+fi
 
 # Remove logs older than 5 days
 find separate_logs/ -type f -mtime +5 -delete || true
