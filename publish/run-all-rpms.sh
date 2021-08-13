2b8311ebe55d2a082ab4df998c20ba699d22bb43
Timo Wilken <timo.wilken@cern.ch>
2021-08-13 08:31:29 +0000

    Post to Mattermost when new RPMs are published (#1044)

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -9,6 +9,8 @@ set -exo pipefail
 arch=$1
 shift   # $@ now contains only the config files to read.
 
+# This contains MATTERMOST_O2_RELEASE_INTEGRATION_URL, which we need.
+. /secrets/ci_secrets
 # aliPublish needs these S3 secrets.
 # rclone gets them from /secrets/alibuild_rclone_config.
 . /secrets/aws_bot_secrets
@@ -40,12 +42,29 @@ while true; do
   pip3 install -r ../requirements.txt
 
   for conf in "$@"; do
+    # Save current list of RPMs so we can see which ones are new later.
+    # Sort it so comm is happy.
+    (cd "/repo/RPMS/$arch" && ls -1 -- *.rpm) | sort > before.pkgs
+
     if "$aliPublish" --config "$conf" --debug sync-rpms >&2; then
       timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose \
               "local:/repo/RPMS/$arch/" "rpms3:alibuild-repo/RPMS/$arch/" || true
+
+      # Compare the file list to the dir now, to see which RPMs were published.
+      (cd "/repo/RPMS/$arch" && ls -1 -- *.rpm) | sort |
+        comm -13 before.pkgs - > new.pkgs
+      # Post in the Release Integration channel if we have new RPMs.
+      if [ "$(wc -l < new.pkgs)" -gt 0 ]; then
+        curl -ifsSX POST -H 'Content-Type: application/json' \
+             -d "{\"text\": \"# New RPMs published for \`$arch\`
+
+$(sed 's/^/- `/; s/$/`/' new.pkgs)\"}" \
+             "$MATTERMOST_O2_RELEASE_INTEGRATION_URL"
+      fi
     else
       echo "ERROR: $aliPublish ($conf) failed with $?; not syncing to S3" >&2
     fi
+    rm -f before.pkgs new.pkgs
   done
 
   # Now that we've uploaded all the new RPMs, we can delete the canary files to
