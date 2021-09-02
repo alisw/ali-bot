6f96b9c6af94c1b919949fb1c39daad06b6d8aaa
Timo Wilken <timo.wilken@cern.ch>
2021-09-02 11:20:37 +0000

    Run aliPublishS3 off S3 repository only (#1056)
    With this patch, aliPublishS3 won't need a full local copy of the entire repo
    any more. Instead, it will merge new RPMs into the S3 repo directly, and upload
    the RPMs directly as well (so the `rclone sync` step isn't needed for
    aliPublishS3 any more).

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -35,37 +35,40 @@ while true; do
   # architecture-specific canary files under rpmstatus/$arch/.
   s3cmd ls "s3://alibuild-repo/rpmstatus/$arch/" | cut -b 32- > canaries.txt
 
+  pip3 install -Ur ../requirements.txt
   case "$arch" in
-    el8.*) aliPublish=./aliPublishS3;;
-    *) aliPublish=./aliPublish;;
-  esac
-  pip3 install -r ../requirements.txt
+    el8.*)
+      for conf in "$@"; do
+        ./aliPublishS3 --config "$conf" --debug sync-rpms
+      done ;;
 
-  for conf in "$@"; do
-    # Save current list of RPMs so we can see which ones are new later.
-    # Sort it so comm is happy.
-    (cd "/repo/RPMS/$arch" && ls -1 -- *.rpm) | sort > before.pkgs
+    *)
+      for conf in "$@"; do
+        # Save current list of RPMs so we can see which ones are new later.
+        # Sort it so comm is happy.
+        (cd "/repo/RPMS/$arch" && ls -1 -- *.rpm) | sort > before.pkgs
 
-    if "$aliPublish" --config "$conf" --debug sync-rpms >&2; then
-      timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose \
-              "local:/repo/RPMS/$arch/" "rpms3:alibuild-repo/RPMS/$arch/" || true
+        if ./aliPublish --config "$conf" --debug sync-rpms; then
+          timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose \
+                  "local:/repo/RPMS/$arch/" "rpms3:alibuild-repo/RPMS/$arch/" || true
 
-      # Compare the file list to the dir now, to see which RPMs were published.
-      (cd "/repo/RPMS/$arch" && ls -1 -- *.rpm) | sort |
-        comm -13 before.pkgs - > new.pkgs
-      # Post in the Release Integration channel if we have new RPMs.
-      if [ "$(wc -l < new.pkgs)" -gt 0 ]; then
-        curl -ifsSX POST -H 'Content-Type: application/json' \
-             -d "{\"text\": \"# New RPMs published for \`$arch\`
+          # Compare the file list to the dir now, to see which RPMs were published.
+          (cd "/repo/RPMS/$arch" && ls -1 -- *.rpm) | sort |
+            comm -13 before.pkgs - > new.pkgs
+          # Post in the Release Integration channel if we have new RPMs.
+          if [ "$(wc -l < new.pkgs)" -gt 0 ]; then
+            curl -ifsSX POST -H 'Content-Type: application/json' \
+                 -d "{\"text\": \"# New RPMs published for \`$arch\`
 
 $(sed 's/^/- `/; s/$/`/' new.pkgs)\"}" \
-             "$MATTERMOST_O2_RELEASE_INTEGRATION_URL"
-      fi
-    else
-      echo "ERROR: $aliPublish ($conf) failed with $?; not syncing to S3" >&2
-    fi
-    rm -f before.pkgs new.pkgs
-  done
+                 "$MATTERMOST_O2_RELEASE_INTEGRATION_URL"
+          fi
+        else
+          echo "ERROR: aliPublish ($conf) failed with $?; not syncing to S3" >&2
+        fi
+        rm -f before.pkgs new.pkgs
+      done ;;
+  esac
 
   # Now that we've uploaded all the new RPMs, we can delete the canary files to
   # tell Jenkins jobs that we're done.
