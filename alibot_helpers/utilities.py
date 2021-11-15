867d8bdb9d9e99f0b6f14c6055b5467074523fbf
Timo Wilken <timo.wilken@cern.ch>
2021-11-15 13:04:37 +0000

    Add script to show an overview of recent PRs (#1041)
    * Extract .env file parsing into utility function
    
    We'll use it in our new script as well.
    
    * Add script to show an overview of recent PRs
    
    The script queries recent pull requests' CI statuses for each known check and
    displays the results in a concise and colourful table for easy analysis.
    
    * Hyperlink each PR number to the corresponding build results
    
    * Pin a specific gql version
    
    And fix the installation for python2.

--- alibot_helpers/utilities.py
+++ alibot_helpers/utilities.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
+
+import shlex
 import sys
 
+
+def parse_env_file(env_file_path):
+    '''Parse variable assignments from a .env file.'''
+    with open(env_file_path) as envf:
+        for token in shlex.split(envf.read(), comments=False):
+            var, is_assignment, value = token.partition('=')
+            if is_assignment:
+                yield (var, value)
+
+
 def to_unicode(s):
-  if sys.version_info[0] >= 3:
-    if isinstance(s, bytes):
-      return s.decode("utf-8")  # to get newlines as such and not as escaped \n
-    return str(s)
-  elif isinstance(s, str):
-    return unicode(s, "utf-8")  # utf-8 is a safe assumption
-  elif not isinstance(s, unicode):
-    return unicode(str(s))
-  return s
+    if sys.version_info[0] >= 3:
+        if isinstance(s, bytes):
+            return s.decode("utf-8")  # to get newlines as such and not as escaped \n
+        return str(s)
+    elif isinstance(s, str):
+        return unicode(s, "utf-8")  # utf-8 is a safe assumption
+    elif not isinstance(s, unicode):
+        return unicode(str(s))
+    return s
