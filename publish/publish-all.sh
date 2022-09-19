fac2ae5f76b274d4aed900258171bed52f4b117c
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-09-19 14:41:59 +0200

    Publish both async and noarch

--- publish/publish-all.sh
+++ publish/publish-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # This is for the rsync repository
 $HOME/publisher/get-and-run.sh
-# This is for the S3 repository
-AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID         \
-AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+# This is for the S3 / el8 repository
 ALIPUBLISH=aliPublishS3 CONF=aliPublish-async.conf $HOME/publisher/get-and-run.sh
+# This is for the noarch repository
+ALIPUBLISH=aliPublishS3 CONF=aliPublish-noarch.conf $HOME/publisher/get-and-run.sh
