alisw/ali-bot

commit 46b0c881621bebfc2f00524d7683d5e3b928c24f

Exclude bookkeeping-api versions with @ from RPM publishing (#1235)
* Exclude bookkeeping-api versions with @ from RPM publishing
* Test new exclude rule

publish/aliPublish-rpms-cc8.conf
@@ -79,6 +79,9 @@ architectures:
         - -18d7f0d6f3-
         - -38c51d6edf-
         - -8112bb1d4c-
+      bookkeeping-api:
+        # These cannot be packaged as RPMs due to the "@" in the version field.
+        - "^bookkeeping@"
 
 # What packages to publish
 auto_include_deps: True

publish/test-rpms-cc8.yaml
@@ -1,0 +1,4 @@
+---
+slc8_x86-64:
+  bookkeeping-api:
+    bookkeeping@0.49.1-1: false

commit 33a9248da1b9bf10f1d1f9db277b29c858b1b1bb

Exclude packages depending on broken bookkeeping version (#1236)
The broken package from #1235 is still being published, as it's being pulled in by other packages. Exclude them, since there's no point in publishing them.

publish/aliPublish-rpms-cc8.conf
@@ -82,6 +82,14 @@ architectures:
       bookkeeping-api:
         # These cannot be packaged as RPMs due to the "@" in the version field.
         - "^bookkeeping@"
+      # The following three depend on the bookkeeping-api version
+      # bookkeeping@0.49.1-1, which can't be published as an RPM.
+      O2PDPSuite:
+        - ^epn-20230308-1$
+      O2sim:
+        - ^v20230308-3$
+      QualityControl:
+        - ^v1.92.0-9$
 
 # What packages to publish
 auto_include_deps: True

publish/test-rpms-cc8.yaml
@@ -1,4 +1,10 @@
 ---
 slc8_x86-64:
+  O2PDPSuite:
+    epn-20230308-1: false
+  O2sim:
+    v20230308-3: false
+  QualityControl:
+    v1.92.0-9: false
   bookkeeping-api:
     bookkeeping@0.49.1-1: false