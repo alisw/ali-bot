b1b0d3c8605f88636d2ac4fa72799b09aeadc9db
Timo Wilken <timo.wilken@cern.ch>
2023-04-19 16:37:32 +0200

    Fix ali-bot installation on recent pip versions
    We need a PEP440-compatible default version number; LAST_TAG is not
    valid and will cause the installation to fail.
    
    Also, the `#egg=ali-bot[ci]` syntax is deprecated and will start to
    fail in future. The recommended replacement is `ali-bot[ci] @ URL`.

--- setup.py
+++ setup.py
@@ -28,10 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    #
-    # LAST_TAG is actually a placeholder which will be automatically replaced by 
-    # the release-alibuild pipeline in jenkins whenever we need a new release.
-    version='LAST_TAG',
+    version='0.0.0',
 
     description='ALICE Multipurpose bot',
     long_description=long_description,
