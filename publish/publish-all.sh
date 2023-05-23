1d94e2a16b7cc86d0a331c2b75ef2cb5c8224af8
Timo Wilken <timo.wilken@cern.ch>
2023-05-23 16:13:45 +0200

    Make the CVMFS publisher take everything from S3

--- publish/publish-all.sh
+++ publish/publish-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # This is for the rsync repository
-$HOME/publisher/get-and-run.sh
+ALIPUBLISH=aliPublishS3 CONF=aliPublish.conf ~/publisher/get-and-run.sh
 # This is for the S3 / el8 and ARM repository
-ALIPUBLISH=aliPublishS3 CONF=aliPublish-s3.conf $HOME/publisher/get-and-run.sh
+ALIPUBLISH=aliPublishS3 CONF=aliPublish-s3.conf ~/publisher/get-and-run.sh
 # This is for the noarch repository
-ALIPUBLISH=aliPublishS3 CONF=aliPublish-noarch.conf $HOME/publisher/get-and-run.sh
+ALIPUBLISH=aliPublishS3 CONF=aliPublish-noarch.conf ~/publisher/get-and-run.sh
