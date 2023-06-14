bbb867154c2afb60d506ada216f516fac32a8c5f
Timo Wilken <timo.wilken@cern.ch>
2023-06-14 14:22:29 +0200

    Merge aliPublish-s3.conf into aliPublish.conf
    This is possible now that aliPublish.conf is used by aliPublishS3, and it
    reduces the number of separate publishers that we need to run.

--- publish/publish-all.sh
+++ publish/publish-all.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
-# This is for the rsync repository
+# This is for the S3 repository.
 ALIPUBLISH=aliPublishS3 CONF=aliPublish.conf ~/publisher/get-and-run.sh
-# This is for the S3 / el8 and ARM repository
-ALIPUBLISH=aliPublishS3 CONF=aliPublish-s3.conf ~/publisher/get-and-run.sh
-# This is for the noarch repository
+# This is for the noarch repository (needs special handling because it
+# overlaps with a stanza in aliPublish.conf).
 ALIPUBLISH=aliPublishS3 CONF=aliPublish-noarch.conf ~/publisher/get-and-run.sh
