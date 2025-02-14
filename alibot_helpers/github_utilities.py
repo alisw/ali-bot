1e81222e3aa3dca588463bd0cebde9887b07d0d3
Sergio Garcia <47090312+singiamtel@users.noreply.github.com>
2025-02-14 11:49:08 +0100

    Improve error message (#1481)
    This happens often in AliPhysics
    
    We should probably reduce the number of updates to the commits somehow
    at some point

--- alibot_helpers/github_utilities.py
+++ alibot_helpers/github_utilities.py
@@ -253,6 +253,7 @@ class GithubCachedClient(object):
         sc = response.status_code
         if sc == 422:
             print(f"GitHub error: Unprocessable Entity", file=sys.stderr)
+            print('This usually means that the commit has reached the maximum number of statuses.')
         return sc
 
     @trace
