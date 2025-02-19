35c068a761a1733779ee6f6d1745a69a252b214f
Sergio Garcia <47090312+singiamtel@users.noreply.github.com>
2025-02-19 22:11:43 +0100

    Cleanup old kibana/elasticsearch/marathon/logstash stuff (#1484)

--- script/set_mappings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ES_HOST=elasticsearch-client.marathon.mesos:9200
-PROXY=${PROXY+--proxy socks5h://$PROXY}
-TARGET=${TARGET-$WORKSPACE/alisw.github.io}
-curl $PROXY -X PUT "http://$ES_HOST/_mapping/logs" -d@es-mappings/logs.json
