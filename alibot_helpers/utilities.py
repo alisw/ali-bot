ecedb0c7dd5216a654a5ad161ba79649687bede8
Sergio Garcia <sergio.garcia.de.la.cruz@cern.ch>
2025-05-19 15:29:10 +0200

    Remove more python2 workarounds

--- alibot_helpers/utilities.py
+++ alibot_helpers/utilities.py
@@ -14,12 +14,6 @@ def parse_env_file(env_file_path):
 
 
 def to_unicode(s):
-    if sys.version_info[0] >= 3:
-        if isinstance(s, bytes):
-            return s.decode("utf-8")  # to get newlines as such and not as escaped \n
-        return str(s)
-    elif isinstance(s, str):
-        return unicode(s, "utf-8")  # utf-8 is a safe assumption
-    elif not isinstance(s, unicode):
-        return unicode(str(s))
-    return s
+    if isinstance(s, bytes):
+        return s.decode("utf-8")  # to get newlines as such and not as escaped \n
+    return str(s)
