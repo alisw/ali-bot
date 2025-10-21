f1527bf902d8a9bc84d93b64d2c0dc4e6bbf7481
Sergio Garcia <sergio.garcia.de.la.cruz@cern.ch>
2025-10-21 17:48:28 +0200

    Add timing information to the publisher

--- publish/get-and-run.sh
+++ publish/get-and-run.sh
@@ -4,6 +4,9 @@
 # spite of `set -e`
 set +o pipefail
 
+TOTAL_START=$(date +%s)
+echo "TIMING: Script started at $(date)"
+
 # State of the last notification. If existing, it contains one line with:
 # last_notification_timestamp consecutive_errors
 NOTIFICATION_STATE_FILE=/tmp/publisher_notification_snoozer
@@ -41,7 +44,12 @@ else
   false
 fi
 : "${CMD:?}" "${DEST=ali-bot}"
-[[ ! -e $DEST/.git ]] && git clone https://github.com/alisw/ali-bot "$DEST"
+if [[ ! -e $DEST/.git ]]; then
+  echo "TIMING: Starting git clone at $(date)"
+  CLONE_START=$(date +%s)
+  git clone https://github.com/alisw/ali-bot "$DEST"
+  echo "TIMING: Git clone took $(($(date +%s) - CLONE_START))s"
+fi
 logdir=${NOMAD_TASK_DIR-$PWD}/log
 mkdir -p "$logdir"
 find "$logdir" -type f -mtime +3 -delete || true
@@ -49,6 +57,8 @@ LOG=$logdir/log-$(date -u +%Y%m%d-%H%M%S%z)
 
 # Export NO_UPDATE to prevent automatic updates
 if [[ ! $NO_UPDATE ]]; then
+  echo "TIMING: Starting git update at $(date)"
+  UPDATE_START=$(date +%s)
   pushd $DEST
     git clean -fd
     git clean -fxd
@@ -57,15 +67,22 @@ if [[ ! $NO_UPDATE ]]; then
     git fetch --tags
     git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
   popd
+  echo "TIMING: Git update took $(($(date +%s) - UPDATE_START))s"
 fi
 
+echo "TIMING: Starting venv setup at $(date)"
+VENV_START=$(date +%s)
 venv=${NOMAD_TASK_DIR-$PWD}/venv.$$
 rm -rf "$venv"
 python3 -m venv "$venv"
+echo "TIMING: Venv creation took $(($(date +%s) - VENV_START))s"
 . "$venv/bin/activate"
 # If DEST is in the cwd, it might be confused for a PyPI package, so make it
 # an absolute path.
+echo "TIMING: Starting pip install at $(date)"
+PIP_START=$(date +%s)
 timeout 300 python3 -m pip install -U "$(realpath "$DEST")"
+echo "TIMING: Pip install took $(($(date +%s) - PIP_START))s"
 
 ln -nfs "$(basename "$LOG.error")" "$logdir/latest"
 cachedir=${NOMAD_TASK_DIR-$PWD}/cache
@@ -76,6 +93,8 @@ pushd $DEST/publish
   ERR=
 
   # Packages publisher
+  echo "TIMING: Starting package publisher at $(date)"
+  PKG_START=$(date +%s)
   ./${ALIPUBLISH:-aliPublishS3} --debug            \
                ${DRYRUN:+--dry-run}                \
                ${NO_NOTIF:+--no-notification}      \
@@ -83,29 +102,41 @@ pushd $DEST/publish
                ${OVERRIDE:+--override "$OVERRIDE"} \
                --cache-deps-dir "$cachedir"        \
                --pidfile /tmp/aliPublish.pid       \
-               $CMD                                \
-               2>&1 | tee -a $LOG.error
+                $CMD                                \
+                2>&1 | tee -a $LOG.error
   [[ ${PIPESTATUS[0]} == 0 ]] || ERR="$ERR packages"
+  echo "TIMING: Package publisher took $(($(date +%s) - PKG_START))s"
 
   # CCDB caching
   if [[ $PUB_CCDB == 1 ]]; then
+    echo "TIMING: Starting CCDB caching at $(date)"
+    CCDB_START=$(date +%s)
     ./cache-ccdb.py cache-ccdb-objects.txt 2>&1 | tee -a "$LOG.error"
     [[ ${PIPESTATUS[0]} == 0 ]] || ERR="$ERR ccdb"
+    echo "TIMING: CCDB caching took $(($(date +%s) - CCDB_START))s"
   fi
 
   # Data publisher (e.g. OADB)
   if [[ $PUB_DATA == 1 ]]; then
+    echo "TIMING: Starting data publisher at $(date)"
+    DATA_START=$(date +%s)
     ./publish-data.sh 2>&1 | tee -a $LOG.error
     [[ ${PIPESTATUS[0]} == 0 ]] || ERR="$ERR data"
+    echo "TIMING: Data publisher took $(($(date +%s) - DATA_START))s"
   fi
 
   # Certificates publisher
   if [[ $PUB_CERT == 1 ]]; then
+    echo "TIMING: Starting certificate publisher at $(date)"
+    CERT_START=$(date +%s)
     ./publish-cert.sh 2>&1 | tee -a $LOG.error
     [[ ${PIPESTATUS[0]} == 0 ]] || ERR="$ERR certificates"
+    echo "TIMING: Certificate publisher took $(($(date +%s) - CERT_START))s"
   fi
 
 popd
+
+echo "TIMING: TOTAL EXECUTION TIME: $(($(date +%s) - TOTAL_START))s"
 
 function notify_on_error() {
   # Policy:
@@ -139,8 +170,11 @@ function notify_on_error() {
   || echo "0    $CONSECUTIVE_ERRORS" > $NOTIFICATION_STATE_FILE
 }
 
+echo "TIMING: Starting cleanup at $(date)"
+CLEANUP_START=$(date +%s)
 deactivate
 rm -rf "$venv"
+echo "TIMING: Cleanup took $(($(date +%s) - CLEANUP_START))s"
 if [[ $ERR ]]; then
   ERR=$(echo $ERR | sed -e 's/ /, /g')
   echo "Something went wrong while publishing: $ERR"
@@ -154,7 +188,9 @@ fi
 
 # Get new version of this script
 if [ -z "$NO_UPDATE" ] && [ -x "$DEST/publish/get-and-run.sh" ]; then
+  echo "TIMING: Copying updated script at $(date)"
   cp -v "$DEST/publish/get-and-run.sh" .
 fi
 
+echo "TIMING: Script finished at $(date)"
 [ -z "$ERR" ]   # exit with failure if any errors occurred
