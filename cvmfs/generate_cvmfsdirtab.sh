0a438bf995fa8943633dd771879851f19383d96d
Sergio Garcia <sergio.garcia.de.la.cruz@cern.ch>
2025-10-29 15:10:31 +0100

    Remove old scripts
    Not used anymore

--- cvmfs/generate_cvmfsdirtab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-cd "$(dirname "$0")/.."
-for D in el* ubuntu*; do
-  [[ -d "$D" ]] || continue
-  echo "/$D/Modules/*"
-  find "$D/Packages" -maxdepth 1 -type d -exec echo '/{}/'* \;
-done > .cvmfsdirtab
-echo All ok
